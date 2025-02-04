37d79ba5cc1ae51c8947285709f80e3fbf3f5c9b
Sujanshrestha <sujanstha5732@gmail.com>
2025-02-04 17:18:56 +0545

    learning

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
-from django.urls import  path
+from django.urls import path
+
 from . import views
+
 urlpatterns = [
-    path("/polls",views.index,name="polls")
+    path("", views.index, name="index"),
+    path("", views.hello, name="hello"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(request,"Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
+def hello(request):
+    return HttpResponse("Hello, world. You're at the polls hello.")
